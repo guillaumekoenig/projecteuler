cf117f0931032785ded7bcc9e62e0e51a97be39e
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2015-09-27 20:53:11 +0200

    factoring remaining calls to sieve

--- prb/035/prb35.hs
+++ prb/035/prb35.hs
@@ -1,13 +1,5 @@
+import Sieve
 import Data.Array.Unboxed
-
-sieveRec :: Int -> [Int] -> [Int] -> [Int]
-sieveRec stop n p
-  | head n > stop = reverse p ++ n
-  | otherwise = sieveRec stop (filter (\x->(x`mod`(head n))/=0) n) (head n : p)
-
-sieve :: Int -> [Int]
-sieve n = sieveRec r [2..n] []
-  where r = (floor . sqrt . fromIntegral) n
 
 rotations :: Int -> [Int]
 rotations n = let a = show n
