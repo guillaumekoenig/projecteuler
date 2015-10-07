991a04dcf9383cc5d9f94e3944cda5a569e12693
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2015-10-07 13:43:10 +0200

    number of sundays on the first in 20th century

---
+++ prb/019/prb19.hs
@@ -0,0 +1,14 @@
+nbDaysInMonth :: Int -> Int
+nbDaysInMonth m = rough - if m`mod`12 == 1 then
+                            if leap then 1 else 2
+                          else 0
+  where
+    rough = [31,30,31,30,31,30,31,31,30,31,30,31]!!(m`mod`12)
+    leap = m`div`12`mod`4 == 0 || m`div`12 == 0
+
+main :: IO ()
+main = print $ snd $ foldl countSundays (dayInit,0) [12..12*101-1]
+  where
+    countSundays (day,count) m = (addMonth day m,count+if day==0 then 1 else 0)
+    dayInit = foldl addMonth 1 [0..11]
+    addMonth day m = (day + nbDaysInMonth m) `mod` 7
