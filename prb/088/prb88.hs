1eca94e68f81d9629d5ee3bce7033bbdcc0563dc
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2015-11-07 22:59:23 +0100

    use uniq from lib in prb88

--- prb/088/prb88.hs
+++ prb/088/prb88.hs
@@ -1,6 +1,7 @@
 import Data.List (sort)
 import qualified Data.Map as DM
 import Divisors
+import Uniq
 
 decomposeProduct :: Int -> [Int] -> Int -> [Int] -> [(Int,Int)]
 decomposeProduct n ds m ds'
@@ -8,11 +9,6 @@ decomposeProduct n ds m ds'
   | m > n  = []
   | otherwise = concatMap f [0..length ds-1]
   where f k = decomposeProduct n (drop k ds) (m * ds !! k) (ds !! k : ds')
-
-uniq :: Eq a => [a] -> [a]
-uniq [] = []
-uniq [x] = [x]
-uniq (x:xs) = let t = uniq xs in if (x == head t) then t else x:t
 
 prb88 :: IO ()
 prb88 = putStrLn.show.sum.uniq.sort $ DM.elems bmap
