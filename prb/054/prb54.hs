f3b66cbda3036ef34a5c44854a0d988891f6d10d
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2015-11-07 22:00:01 +0100

    poker hands

---
+++ prb/054/prb54.hs
@@ -0,0 +1,36 @@
+import Data.Ord
+import Data.List
+import Data.Maybe
+
+eq :: Eq b => (a -> b) -> a -> a -> Bool
+eq f a b = f a == f b
+
+handValue :: [(Char,Char)] -> [Int]
+handValue hand = handForce:(map (fst.head) step2)
+  where as = "23456789TJQKA"
+        step1 = sort' $ map (\(a,b)->(fromJust $ elemIndex a as,b)) hand
+          where sort' = sortBy (flip $ comparing fst)
+        step2 = sort' $ groupBy (eq fst) step1
+          where sort' = sortBy (flip $ comparing length)
+        sameSuit = all (eq snd (head step1)) $ tail step1
+        consecutive = all (==1) $ zipWith (-) values (tail values)
+          where values = map fst step1
+        royalFlush = sameSuit && map fst step1 == [14,13..10]
+        straightFlush = sameSuit && consecutive
+        fourOfAKind = map length step2 == [4,1]
+        fullHouse = map length step2 == [3,2]
+        flush = sameSuit
+        straight = consecutive
+        threeOfAKind = map length step2 == [3,1,1]
+        twoPairs = map length step2 == [2,2,1]
+        onePair = map length step2 == [2,1,1,1]
+        whichHand = [royalFlush,straightFlush,fourOfAKind,fullHouse,
+                     flush,straight,threeOfAKind,twoPairs,onePair,True]
+        handForce = 9 - (fromJust $ findIndex id whichHand)
+
+main :: IO ()
+main = readFile "p054_poker.txt" >>= parse >>= run >>= print
+  where parse = pure . map (splitAt 5 . (map tuplify) . words) . lines
+          where tuplify [x,y] = (x,y); tuplify _ = error "Parse error"
+        run = pure . length . filter hand1Wins
+          where hand1Wins (h1,h2) = comparing handValue h1 h2 == GT
