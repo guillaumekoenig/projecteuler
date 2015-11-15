db6771be332b847b11b9b7e0b503de973180847c
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2015-11-15 19:59:53 +0100

    use vector instead of list (for fast lookups)

--- prb/059/prb59.hs
+++ prb/059/prb59.hs
@@ -1,6 +1,6 @@
-import Data.Ord
-import Data.List
-import Data.Bits
+import Data.Char (ord)
+import Data.Bits (xor)
+import qualified Data.Vector as V
 
 -- ^ Seperate elements based on their index modulo n.
 -- Unfortunately, does not work if length xs is not a
@@ -9,21 +9,14 @@ listMod :: [a] -> Int -> [[a]]
 listMod [] n = take n $ repeat []
 listMod xs n = zipWith (:) (take n xs) (listMod (drop n xs) n)
 
--- ^ This is O(n^2) which is pretty bad. It can be
--- made O(n) if lookup was constant. Not sure how to
--- do this in Haskell yet.
-mostFrequent :: Eq a => [a] -> [(a,Int)] -> (a,Int)
-mostFrequent [] cs = maximumBy (comparing snd) cs
-mostFrequent xs cs = mostFrequent (tail xs) cs'
-  where cs' = case findIndex ((==head xs).fst) cs of
-          Just i -> (head xs,(+1).snd$cs!!i):(take i cs++drop (i+1) cs)
-          Nothing -> (head xs,1):cs
+mostFrequent :: [Int] -> Int
+mostFrequent xs = V.maxIndex $ V.accum (+) zeros $ zip xs [1,1..]
+  where zeros = V.replicate 256 (0::Int)
 
 prb59 :: [Int] -> Int
 prb59 xs = sum $ zipWith xor xs (cycle key)
-  where -- 32 is ascii value for space, the most
-        -- frequently appearing caracter
-        key = map (\zs->xor 32 $ fst $ mostFrequent zs []) ys
+  where -- space is the most frequent caracter
+        key = map (\zs->xor (ord ' ') $ mostFrequent zs) ys
         -- listMod does not work currently if
         -- length is not a multiple of n, which is
         -- why the last element is left out (init)
