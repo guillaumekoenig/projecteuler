15f2f06f0ffe78bc778acac05fc3ea1ddf7c1912
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2016-06-09 23:18:11 +0200

    Fix broken divisors function
    This is embarrassing and shows the need for tests.

--- lib/Divisors.hs
+++ lib/Divisors.hs
@@ -1,9 +1,13 @@
 module Divisors (divisors) where
 
-import Isqrt
+import Isqrt (isqrt, isSquare)
+
+divisors' :: Int -> Int -> [Int]
+divisors' n lim = foldr both [] $ filter divisor [2..lim]
+  where both m acc = m:(n`div`m):acc
+        divisor m = n`mod`m==0
 
 divisors :: Int -> [Int]
-divisors n = if isqrt n*isqrt n==n then isqrt n:ds else ds
-  where ds = foldr both [] $ filter divisor [2..isqrt n-1]
-        both m acc = m:(n`div`m):acc
-        divisor m = n`mod`m==0
+divisors n
+  | isSquare n = isqrt n : divisors' n (isqrt n-1)
+  | otherwise = divisors' n (isqrt n)
