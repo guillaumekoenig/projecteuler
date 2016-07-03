4d41d543781e0b432f585aa53d3dbb8928825b63
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2016-07-03 17:27:28 +0200

    Reciprocal cycles

---
+++ prb/026/prb26.hs
@@ -0,0 +1,23 @@
+-- Repeating decimals can be converted back to fractions by observing
+--   1/9   = 0.111111111...
+--   1/99  = 0.010101010...
+--   1/999 = 0.001001001...
+-- If the cycle is 142857, we have
+--   0.142857142857... = 1/999999 * 142857
+--     = 1/7
+-- Ie 999999 = 0 mod 7
+--      10^k = 1 mod 7
+import IsPrime
+import PowerMod
+import Data.List
+import Data.Ord
+import Data.Maybe
+
+prb26 :: Int
+prb26 = fst $ maximumBy (comparing snd) $ map cycLen primes
+  where cycLen d = (,) d $ fromJust $ find (lenMatch d) [1..]
+        lenMatch d k = powerMod 10 k d == 1
+        primes = filter isPrime $ 3:[7..1000]
+
+main :: IO ()
+main = print $ prb26
