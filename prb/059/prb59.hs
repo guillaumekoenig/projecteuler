12822074ed8981c57b16bf869ce2c4d587295746
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2016-07-26 20:07:23 +0200

    Update prb59

--- prb/059/prb59.hs
+++ prb/059/prb59.hs
@@ -2,12 +2,14 @@ import Data.Char (ord)
 import Data.Bits (xor)
 import qualified Data.Vector as V
 
--- ^ Seperate elements based on their index modulo n.
--- Unfortunately, does not work if length xs is not a
--- multiple of n (because of the use of zip).
-listMod :: [a] -> Int -> [[a]]
-listMod [] n = take n $ repeat []
-listMod xs n = zipWith (:) (take n xs) (listMod (drop n xs) n)
+zip' :: [a] -> [[a]] -> [[a]]
+zip' [] ys = ys
+zip' xs [] = map (\x -> [x]) xs
+zip' (x:xs) (y:ys) = (x:y):zip' xs ys
+
+groupIndexMod :: Int -> [a] -> [[a]]
+groupIndexMod n [] = take n $ repeat []
+groupIndexMod n xs = zip' (take n xs) (groupIndexMod n (drop n xs))
 
 mostFrequent :: [Int] -> Int
 mostFrequent xs = V.maxIndex $ V.accum (+) zeros $ zip xs [1,1..]
@@ -15,12 +17,9 @@ mostFrequent xs = V.maxIndex $ V.accum (+) zeros $ zip xs [1,1..]
 
 prb59 :: [Int] -> Int
 prb59 xs = sum $ zipWith xor xs (cycle key)
-  where -- space is the most frequent caracter
-        key = map (\zs->xor (ord ' ') $ mostFrequent zs) ys
-        -- listMod does not work currently if
-        -- length is not a multiple of n, which is
-        -- why the last element is left out (init)
-        ys = listMod (init xs) 3
+  where key = map (\y -> xor mostFreqChar (mostFrequent y)) ys
+        ys = groupIndexMod 3 xs
+        mostFreqChar = ord ' '
 
 main :: IO ()
 main = readFile "p059_cipher.txt" >>= parse >>= run >>= print
