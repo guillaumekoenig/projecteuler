f6e9b704c25816c8c83061ec1298ca8e068e0d38
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2016-08-27 20:14:30 +0200

    Truncatable primes
    95% of time is spent in the initial primes call (not in isPrime calls on
    removeRight or removeLeft).

--- lib/IsPrime.hs
+++ lib/IsPrime.hs
@@ -1,4 +1,4 @@
-module IsPrime (isPrime) where
+module IsPrime (isPrime, primes) where
 
 import Isqrt
 
@@ -7,3 +7,16 @@ isPrime n
   | n <= 3 = n > 1
   | n`mod`2 == 0 || n`mod`3 == 0 = False
   | otherwise = not $ or [n`mod`i == 0 || n`mod`(i+2) == 0|i<-[5,11..isqrt n]]
+
+primes :: [Int]
+primes = filter isPrime [2..]
+
+-- ^ Your average sieve of eratosthenes
+-- Problem is it's slower than filtering
+-- with isPrime on [2..10^6]
+-- primesUpTo :: Int -> [Int]
+-- primesUpTo n = map fst $ filter snd $ runST $ do
+--   arr <- newArray (2,n) True :: ST s (STUArray s Int Bool)
+--   let markFalse = \i -> writeArray arr i False
+--   mapM_ (\x->mapM_ markFalse [2*x,3*x..n]) [2..isqrt n]
+--   getAssocs arr
---
+++ prb/037/prb37.hs
@@ -0,0 +1,20 @@
+import IsPrime (primes, isPrime)
+
+removeRight :: Int -> [Int]
+removeRight n
+  | n`div`10 == 0 = []
+  | otherwise = n`div`10 : removeRight (n`div`10)
+
+removeLeft :: Int -> [Int]
+removeLeft n' = remove n' 10
+  where remove n m
+          | n > m = n`mod`m : remove n (10*m)
+          | otherwise = []
+
+prb37 :: Int
+prb37 = sum $ take 11 $ filter p $ dropWhile (<10) $ primes
+  where p x = and (map isPrime $ removeRight x) &&
+              and (map isPrime $ removeLeft x)
+
+main :: IO ()
+main = print $ prb37
