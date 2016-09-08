3be6a7141acf8711c9d3b16dd7d47f05a657912c
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2016-09-08 22:08:29 +0200

    Factorize perms function to generate permutations

---
+++ lib/Permutations.hs
@@ -0,0 +1,7 @@
+module Permutations (perms) where
+
+perms :: [a] -> [[a]]
+perms xs = perms' xs []
+  where perms' [] partial = [partial]
+        perms' from partial = concatMap (\i->recur $ splitAt i from) [0..length from-1]
+          where recur (beg,end) = perms' (beg ++ tail end) (partial ++ [head end])
--- prb/032/prb32.hs
+++ prb/032/prb32.hs
@@ -1,13 +1,9 @@
 import Data.List (nub)
-
-perms :: [a] -> [a] -> [[a]]
-perms [] partial = [partial]
-perms from partial = concatMap (\i->recur $ splitAt i from) [0..length from-1]
-  where recur (beg,end) = perms (beg ++ tail end) (partial ++ [head end])
+import Permutations (perms)
 
 prb32 :: IO ()
 prb32 = putStrLn $ show $ sum $ nub $ map (\(_,_,c)->c) res
-  where res = filter keep $ concatMap count $ perms "123456789" ""
+  where res = filter keep $ concatMap count $ perms "123456789"
         count xs = map extract [test1 xs,test2 xs]
         test1 xs = let y=splitAt 1 xs;z=splitAt 4 $ snd y in (fst y,fst z,snd z)
         test2 xs = let y=splitAt 2 xs;z=splitAt 3 $ snd y in (fst y,fst z,snd z)
