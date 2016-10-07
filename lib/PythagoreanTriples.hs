2d413f73fc14ec1a8bc574f8ea5afbd67d1d0d4d
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2016-10-08 00:41:23 +0200

    Speed up prb75
    Increased speed comes from marking multiples of perimeter in a sieve
    instead of creating the complete list of pythagorean triples (primitive
    ones and non primitive ones), and using that sieve instead of the slow
    sort + group combo.

--- lib/PythagoreanTriples.hs
+++ lib/PythagoreanTriples.hs
@@ -2,8 +2,15 @@
 -- http://mathworld.wolfram.com/PythagoreanTriple.html
 -- (suggested in problem 39's solution thread)
 
+-- Note: creating long lists is slow (I suppose because of the
+-- need to allocate and run the garbage collector). Dealing
+-- with each triple once we have it is faster (tested by
+-- injecting this code in prb75). It should be possible to do
+-- that with a callback, but what should its type be ? What I
+-- want is write in a mutable array...
+
 module PythagoreanTriples (
-  pythagoreanTriplePrimitives,
+  pythagoreanPrims,
   pythagoreanTriples
 ) where
 
@@ -17,8 +24,8 @@ mU (a,b,c) = (a-2*b+2*c,2*a-b+2*c,2*a-2*b+3*c)
 mA (a,b,c) = (a+2*b+2*c,2*a+b+2*c,2*a+2*b+3*c)
 mD (a,b,c) = (-a+2*b+2*c,-2*a+b+2*c,-2*a+2*b+3*c)
 
-pythagoreanTriplePrimitives :: (Triple -> Bool) -> [Triple]
-pythagoreanTriplePrimitives insideBound = go (3,4,5) []
+pythagoreanPrims :: (Triple -> Bool) -> [Triple]
+pythagoreanPrims insideBound = go (3,4,5) []
   where go (a,b,c) acc
           | not $ insideBound (a,b,c) = acc
           | otherwise = (go (mD (a,b,c))
@@ -27,6 +34,6 @@ pythagoreanTriplePrimitives insideBound = go (3,4,5) []
 
 pythagoreanTriples :: (Triple -> Bool) -> [Triple]
 pythagoreanTriples insideBound = concatMap multiples primitives
-  where primitives = pythagoreanTriplePrimitives insideBound
+  where primitives = pythagoreanPrims insideBound
         multiples (a,b,c) = (a,b,c) : (takeWhile insideBound $
                                        map (\k->(k*a,k*b,k*c)) [2..])
--- prb/075/prb75.hs
+++ prb/075/prb75.hs
@@ -1,10 +1,21 @@
 import PythagoreanTriples
-import Data.List (group, sort)
+import Data.Array.ST
+import Data.Array.Base
+import Control.Monad
 
 prb75 :: Int
-prb75 = length $ filter (\xs -> length xs==1) $
-        group $ sort $ map (\(a,b,c) -> a+b+c) $
-        pythagoreanTriples (\(a,b,c) -> a+b+c<=1500000)
+prb75 = length $ filter (==1) $ elems $ runSTUArray $ do
+  let pmax = 1500000
+  arr <- newArray (0,pmax) (0::Int)
+  let perim (a,b,c) = a+b+c
+      mark p = go p p
+      go = \p pm -> do
+        when (pm <= pmax) $ do
+          c <- unsafeRead arr pm
+          unsafeWrite arr pm (c+1)
+          go p (pm+p)
+  mapM_ (mark . perim) (pythagoreanPrims ((<=pmax) . perim))
+  return arr
 
 main :: IO ()
 main = print $ prb75
