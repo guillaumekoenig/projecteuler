07a4f23671ab5855c69ce6fff6933c4514d63221
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2017-03-20 00:38:20 +0100

    Rewrite sieve of Eratosthenes

--- lib/IsPrime.hs
+++ lib/IsPrime.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module IsPrime (isPrime, primesTo) where
 
-import Isqrt
+import Isqrt (isqrt)
 
-import Data.Array.ST
-import Data.Array.Base
-import Control.Monad
+import Data.Array.ST (runSTUArray)
+import Data.Array.Base (newArray, assocs, unsafeRead, unsafeWrite)
 
 isPrime :: Int -> Bool
 isPrime n
@@ -13,24 +14,29 @@ isPrime n
   | otherwise = not $ or [n`mod`i == 0 || n`mod`(i+2) == 0|i<-[5,11..isqrt n]]
 
 -- ^ Your average sieve of eratosthenes
--- runSTUArray combined with unsafe{Read,Write} is much
--- faster than runST with readArray and writeArray. Note
+-- runSTUArray combined with unsafe{Read,Write} is a bit
+-- faster than used with {read,write}Array. Note that
 -- the latters do bounds checking contrary to the unsafe
 -- variants. Also, the unsafe variants have their index
 -- starting at 0, not the lower bound index defined when
 -- creating the array.
 primesTo :: Int -> [Int]
-primesTo n = map fst $ filter snd $ assocs $ runSTUArray $ do
-  arr <- newArray (0,n) True
-  unsafeWrite arr 0 False
-  unsafeWrite arr 1 False
-  let mark = \i -> do
-        i0 <- unsafeRead arr i
-        when i0 $ markFalse (i+i) i
-        when (i+1 <= isqrt n) $ mark (i+1)
-      markFalse = \start step ->
-        when (start <= n) $ do
-          unsafeWrite arr start False
-          markFalse (start+step) step
-  mark 2
+primesTo n = map fst . filter (not . snd) . assocs $ runSTUArray $ do
+  arr <- newArray (0,n) False -- Slightly faster than with True (?)
+  unsafeWrite arr 0 True
+  unsafeWrite arr 1 True
+  go arr 2
   return arr
+    where go a i
+            | i > isqrt n = return ()
+            | otherwise = do
+                notprime <- unsafeRead a i
+                if not notprime
+                  then mark a (i+i) i
+                  else return ()
+                go a (i+1)
+          mark a i s
+            | i > n = return ()
+            | otherwise = do
+                unsafeWrite a i True
+                mark a (i+s) s
