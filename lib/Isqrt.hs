b8b3d74991f1565a3ffe9917cb1b716922d1118b
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2017-03-19 20:02:29 +0100

    Prime power triples

--- lib/Isqrt.hs
+++ lib/Isqrt.hs
@@ -1,7 +1,11 @@
-module Isqrt (isqrt,isSquare) where
+module Isqrt (isqrt, isSquare, isqrtn) where
 
 isqrt :: Int -> Int
 isqrt = floor . (sqrt :: Double -> Double) . fromIntegral
 
 isSquare :: Int -> Bool
 isSquare n = let r = isqrt n in r*r == n
+
+isqrtn :: Int -> Int -> Int
+isqrtn n = floor . (** (1/n')) . fromIntegral
+  where n' = fromIntegral n :: Double
