59d575bc7cb32ef5883838885e4caae8d1e3a098
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2017-04-17 19:22:19 +0200

    Investigate further pythagoreanPrims{,2}

--- lib/PythagoreanTriples.hs
+++ lib/PythagoreanTriples.hs
@@ -1,18 +1,22 @@
--- Generate pythagorean triples based on:
+-- Generate pythagorean primitives based on:
 -- http://mathworld.wolfram.com/PythagoreanTriple.html
 -- (suggested in problem 39's solution thread)
 
--- Note: creating long lists is slow (I suppose because of the
--- need to allocate and run the garbage collector). Dealing
--- with each triple once we have it is faster (tested by
--- injecting this code in prb75). It should be possible to do
--- that with a callback, but what should its type be ? What I
--- want is write in a mutable array...
+-- All triples are obtained by also considering the
+-- multiples of primitives.
+
+-- Both pythagoreanPrims{,2} produce lists that can
+-- be used lazily. This can be seen by applying length
+-- and watching memory remain constant regardless of
+-- the number of items.
+
+-- However, they return the items in very different
+-- orders. Depending on the use afterwards, one will
+-- provide better locality and hence be better on the
+-- caches.
 
 module PythagoreanTriples (
   pythagoreanPrims,
-  pythagoreanTriples,
-
   pythagoreanPrims2,
 ) where
 
@@ -27,29 +31,18 @@ mA (a,b,c) = (a+2*b+2*c,2*a+b+2*c,2*a+2*b+3*c)
 mD (a,b,c) = (-a+2*b+2*c,-2*a+b+2*c,-2*a+2*b+3*c)
 
 pythagoreanPrims :: (Triple -> Bool) -> [Triple]
-pythagoreanPrims insideBound = go (3,4,5) []
+pythagoreanPrims insideBounds = go (3,4,5) []
   where go (a,b,c) acc
-          | not $ insideBound (a,b,c) = acc
+          | (not . insideBounds) (a,b,c) = acc
           | otherwise = (go (mD (a,b,c))
-                         (go (mA (a,b,c))
-                          (go (mU (a,b,c)) ((a,b,c):acc))))
-
-pythagoreanTriples :: (Triple -> Bool) -> [Triple]
-pythagoreanTriples insideBound = concatMap multiples primitives
-  where primitives = pythagoreanPrims insideBound
-        multiples (a,b,c) = (a,b,c) : (takeWhile insideBound $
-                                       map (\k->(k*a,k*b,k*c)) [2..])
+                          (go (mA (a,b,c))
+                            (go (mU (a,b,c)) ((a,b,c):acc))))
 
 -- Formula from the "early Greeks", again found on:
 -- http://mathworld.wolfram.com/PythagoreanTriple.html
 pythagoreanPrims2 :: Int -> [Triple]
-pythagoreanPrims2 maxPerim = [(a,b,c)
-                             |u<-[1..lim1],
-                              v<-[u+1,u+3..lim2],
-                              gcd u v==1,
-                              let a=v*v-u*u,
-                              let b=2*u*v,
-                              let c=u*u+v*v,
-                              a+b+c<=maxPerim]
-  where lim1 = floor $ sqrt (fromIntegral maxPerim/4 :: Double)
-        lim2 = floor $ sqrt (fromIntegral maxPerim :: Double)
+pythagoreanPrims2 maxPerim = filter (\(a,b,c)->a+b+c<=maxPerim)
+  [triple (u,v) | u<-[1..lim1],v<-[u+1,u+3..lim2],gcd u v==1]
+  where lim1 = (floor . sqrt) (fromIntegral maxPerim/4 :: Double)
+        lim2 = (floor . sqrt) (fromIntegral maxPerim :: Double)
+        triple (u,v) = (v*v-u*u,2*u*v,u*u+v*v)
