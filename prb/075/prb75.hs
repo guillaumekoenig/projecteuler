5d7c173cb9f21631442439cf8b741809b0d3b3c5
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2017-04-18 01:30:00 +0200

    Rewrite problem 75 to use array in ST monad

--- prb/075/prb75.hs
+++ prb/075/prb75.hs
@@ -1,41 +1,33 @@
-import Control.Monad
-import Data.Array.IO
-import PythagoreanTriples
+import Data.Int (Int8)
+import Data.Array.ST (runSTUArray)
+import Data.Array.Base (newArray, unsafeRead, unsafeWrite, elems)
+import PythagoreanTriples (pythagoreanPrims2)
 
-prb75 :: IO Int
-prb75 = do
-  let pmax = 1500000
-  -- Switching from ST to regular IO actions. ST is
-  -- too much magic at this point. Nice articles on IO:
-  -- https://wiki.haskell.org/Introduction_to_IO
-  -- http://www.haskell.org/haskellwiki/IO_inside
-  arr <- newArray (0,pmax) 0 :: IO (IOUArray Int Int)
-  let perim (a,b,c) = a+b+c
-      mark p = go p p
-      go = \p pm -> do
-        when (pm <= pmax) $ do
-          c <- readArray arr pm
-          writeArray arr pm (c+1)
-          go p (pm+p)
-  mapM_ (mark . perim) (pythagoreanPrims2 pmax)
-  -- I don't understand why `let count2 = \i acc ->`
-  -- is valid, but `let count2 i acc =` is not (?)
-  let count = \i acc -> do
-        if (i > pmax)
-          then return acc
-          else do
-            c <- readArray arr i
-            let acc' = (if c == 1 then 1 else 0) + acc
-            -- As it turns out this is a classical example
-            -- of lazy evaluation degrading performance. We're
-            -- basically doing a left fold here. If we were not
-            -- forcing acc' to be evaluated (through seq), the
-            -- expression would grow unreduced until the end of
-            -- the recursion, when the actual value would be
-            -- needed and reduced. See this link on the topic:
-            -- https://hackhands.com/lazy-evaluation-works-haskell/
-            acc' `seq` (count (i+1) acc')
-  count 0 0
+-- Counting with unboxed array is decently fast. Using Int8
+-- is faster than Int as it uses less memory and hence makes
+-- better use of caches. An even better approach would be to
+-- use two bits only to identify unique triples (maybe with
+-- an unboxed Bool array, which is packed in memory).
+-- A worse problem is the list of candidates being built up
+-- fully in memory instead of being used on the fly. Regular
+-- foldl keeps memory down, but neither does mapM nor foldM.
+-- Maybe linked to monadic setting. Of course less memory
+-- means faster execution. I don't know how to fix except by
+-- recurring directly, without an intermediate list.
+prb75 :: Int
+prb75 = length . filter (==1) . elems $ runSTUArray $ do
+  arr <- newArray (0,pmax) (0::Int8)
+  mapM_ (mark arr . perimeter) (pythagoreanPrims2 pmax)
+  return arr
+    where pmax = 1500000
+          perimeter (a,b,c) = a+b+c
+          mark arr p = go arr p p
+          go arr p pm = do
+            if pm <= pmax
+              then do c <- unsafeRead arr pm
+                      unsafeWrite arr pm (min (c+1) 2)
+                      go arr p (pm+p)
+              else return ()
 
 main :: IO ()
-main = print =<< prb75
+main = print prb75
