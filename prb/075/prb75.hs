663b7e312e640e78bcd2065999f39f4f4b1a3a3d
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2017-04-18 08:36:43 +0200

    Use foldM instead of mapM_ in problem 75

--- prb/075/prb75.hs
+++ prb/075/prb75.hs
@@ -1,6 +1,7 @@
 import Data.Int (Int8)
 import Data.Array.ST (runSTUArray)
 import Data.Array.Base (newArray, unsafeRead, unsafeWrite, elems)
+import Control.Monad (foldM)
 import PythagoreanTriples (pythagoreanPrims2)
 
 -- Counting with unboxed array is decently fast. Using Int8
@@ -8,17 +9,20 @@ import PythagoreanTriples (pythagoreanPrims2)
 -- better use of caches. An even better approach would be to
 -- use two bits only to identify unique triples (maybe with
 -- an unboxed Bool array, which is packed in memory).
+
 -- A worse problem is the list of candidates being built up
 -- fully in memory instead of being used on the fly. Regular
 -- foldl keeps memory down, but neither does mapM nor foldM.
 -- Maybe linked to monadic setting. Of course less memory
 -- means faster execution. I don't know how to fix except by
 -- recurring directly, without an intermediate list.
+
+-- TODO investigate foldl vs foldM
+
 prb75 :: Int
 prb75 = length . filter (==1) . elems $ runSTUArray $ do
   arr <- newArray (0,pmax) (0::Int8)
-  mapM_ (mark arr . perimeter) (pythagoreanPrims2 pmax)
-  return arr
+  foldM (\a -> mark a . perimeter) arr (pythagoreanPrims2 pmax)
     where pmax = 1500000
           perimeter (a,b,c) = a+b+c
           mark arr p = go arr p p
@@ -27,7 +31,7 @@ prb75 = length . filter (==1) . elems $ runSTUArray $ do
               then do c <- unsafeRead arr pm
                       unsafeWrite arr pm (min (c+1) 2)
                       go arr p (pm+p)
-              else return ()
+              else return arr
 
 main :: IO ()
 main = print prb75
