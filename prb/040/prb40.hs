c7660fbfff2c3cbe4e702d20fec3248978900a86
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2017-04-22 16:28:33 +0200

    Use recursive list instead of recursive function
    Just because now I know how to write them and how they are evaluted
    under lazy evaluation.

--- prb/040/prb40.hs
+++ prb/040/prb40.hs
@@ -1,10 +1,9 @@
-steps :: [Int]
-steps = go 1 1
-  where go x d = x : go (x+(10^d-10^(d-1))*d) (d+1)
+steps :: [(Int,Int)]
+steps = (1,1) : [(d+1,s+(10^d-10^(d-1))*d) | (d,s)<-steps]
 
 digitAtPos :: Int -> Int
 digitAtPos n = extract (d-1-(n-s)`mod`d) ((n-s)`div`d+10^(d-1))
-  where (d,s) = (last . zip [1..] . takeWhile (<=n)) steps
+  where (d,s) = (last . takeWhile ((<=n) . snd)) steps
         extract i num = num`div`(10^i)`mod`10
 
 main :: IO ()
