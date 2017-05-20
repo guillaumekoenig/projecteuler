d6939c14c57639e711095250ec538fbcd8f0d682
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2017-05-20 15:30:13 -0400

    Prime pair sets 10x speedup
    This is done by cutting down on the number of tests to perform before
    reaching the result.

--- Lib/IsPrime.hs
+++ Lib/IsPrime.hs
@@ -7,11 +7,12 @@ import Lib.Isqrt (isqrt)
 import Data.Array.ST (runSTUArray)
 import Data.Array.Base (newArray, assocs, unsafeRead, unsafeWrite)
 
+-- Note: `rem` faster than `mod`
 isPrime :: Int -> Bool
 isPrime n
   | n <= 3 = n > 1
-  | n`mod`2 == 0 || n`mod`3 == 0 = False
-  | otherwise = not $ or [n`mod`i == 0 || n`mod`(i+2) == 0|i<-[5,11..isqrt n]]
+  | n`rem`2 == 0 || n`rem`3 == 0 = False
+  | otherwise = not $ or [n`rem`i == 0 || n`rem`(i+2) == 0|i<-[5,11..isqrt n]]
 
 primeFactors :: Int -> [Int]
 primeFactors n
