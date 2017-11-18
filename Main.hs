4819fd6c77a972d0e04987985e51c407243df092
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2017-11-18 18:13:07 +0100

    Add individual timings and sort with most time consuming first

--- Main.hs
+++ Main.hs
@@ -1,79 +1,45 @@
+module Main where
+
+import Data.Time.Clock
+import Data.Ord
+import Data.List
+
 import Problems
 
+empty :: IO Int
+empty = return 0
+
+prbs :: [IO Int]
+prbs = [prb1, prb2, prb3, prb4, prb5, prb6, prb7, prb8, prb9, prb10,
+  prb11, prb12, prb13, prb14, prb15, prb16, prb17, prb18, prb19, prb20,
+  prb21, prb22, prb23, prb24, prb25, prb26, prb27, prb28, prb29, prb30,
+  prb31, prb32, prb33, prb34, prb35, prb36, prb37, prb38, prb39, prb40,
+  prb41, prb42, prb43, prb44, prb45, prb46, prb47, prb48, prb49, prb50,
+  prb51, prb52, prb53, prb54, prb55, prb56, prb57, prb58, prb59, prb60,
+  prb61, empty, empty, empty, prb65, empty, prb67, empty, empty, empty,
+  empty, empty, empty, empty, prb75, empty, empty, empty, empty, empty,
+  prb81, prb82, prb83, empty, prb85, prb86, prb87, prb88, empty, prb90,
+  empty, empty, empty, empty, empty, empty, prb97, prb98, prb99, prb100]
+
+runAll :: [IO Int] -> IO [(Int,NominalDiffTime)]
+runAll [] = return []
+runAll (p:ps) = do start <- getCurrentTime
+                   r <- p
+                   let !result = r
+                   end <- getCurrentTime
+                   let time = diffUTCTime end start
+                   rest <- runAll ps
+                   return $ (result,time):rest
+
+thd :: (a,b,c) -> c
+thd (_,_,x) = x
+
 main :: IO ()
-main = do print =<< prb1
-          print =<< prb2
-          print =<< prb3
-          print =<< prb4
-          print =<< prb5
-          print =<< prb6
-          print =<< prb7
-          print =<< prb8
-          print =<< prb9
-          print =<< prb10
-          print =<< prb11
-          print =<< prb12
-          print =<< prb13
-          print =<< prb14
-          print =<< prb15
-          print =<< prb16
-          print =<< prb17
-          print =<< prb18
-          print =<< prb19
-          print =<< prb20
-          print =<< prb21
-          print =<< prb22
-          print =<< prb23
-          print =<< prb24
-          print =<< prb25
-          print =<< prb26
-          print =<< prb27
-          print =<< prb28
-          print =<< prb29
-          print =<< prb30
-          print =<< prb31
-          print =<< prb32
-          print =<< prb33
-          print =<< prb34
-          print =<< prb35
-          print =<< prb36
-          print =<< prb37
-          print =<< prb38
-          print =<< prb39
-          print =<< prb40
-          print =<< prb41
-          print =<< prb42
-          print =<< prb43
-          print =<< prb44
-          print =<< prb45
-          print =<< prb46
-          print =<< prb47
-          print =<< prb48
-          print =<< prb49
-          print =<< prb50
-          print =<< prb51
-          print =<< prb52
-          print =<< prb53
-          print =<< prb54
-          print =<< prb55
-          print =<< prb56
-          print =<< prb57
-          print =<< prb58
-          print =<< prb59
-          print =<< prb60
-          print =<< prb61
-          print =<< prb65
-          print =<< prb67
-          print =<< prb75
-          print =<< prb81
-          print =<< prb82
-          print =<< prb83
-          print =<< prb85
-          print =<< prb86
-          print =<< prb87
-          print =<< prb88
-          print =<< prb90
-          print =<< prb97
-          print =<< prb98
-          print =<< prb99
-          print =<< prb100
+main = do results <- runAll prbs
+          let labeled = zipWith (\a (b,c)->(a,b,c)) [1..] results
+          let orderDesc = sortBy (flip $ comparing thd) labeled
+          let totalTime = sum $ map snd results
+          let pp (n,r,t) = "Prb #" ++ show n ++ ": " ++ show r
+                ++ "    \t(" ++ show t ++ ")"
+          mapM_ (putStrLn . pp) orderDesc
+          putStrLn $ "\nTotal: " ++ show totalTime
