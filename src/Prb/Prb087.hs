1ef1801aa10cb618dbd7e8c8a8dea8a3db1e4cd2
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2018-08-15 19:48:29 +0200

    Stylistic change to problem 87

--- src/Prb/Prb087.hs
+++ src/Prb/Prb087.hs
@@ -1,8 +1,7 @@
 module Prb.Prb087 (prb87) where
 
--- You can get out of monad via runST for the ST
--- monad, and cannot get out of it for the IO monad
--- (on purpose)
+-- You can get out of monad via runST for the ST monad, and cannot get
+-- out of it for the IO monad (on purpose)
 
 -- This was useful: https://wiki.haskell.org/Monad/ST
 
@@ -13,17 +12,15 @@ import Lib.Isqrt (isqrtn)
 target :: Int
 target = 50000000
 
--- Array/vector: the immutable versions require
--- to copy the array/vector for each update which
--- is very inefficient; mutable ones are fast but
--- harder to work out (in the ST or IO monad)
--- IntSet (3.5 s) < IntMap (4 s) < Sequence (10 s)
--- Sequence not very good, because of sparse data
--- (The ratio of data over holes is roughly 1/50)
--- BitArray ... 150 ms
--- Bool STUArray ... 120 ms (likely binary packed)
+-- Array/vector: the immutable versions require to copy the
+-- array/vector for each update which is very inefficient; mutable
+-- ones are fast but harder to work out (in the ST or IO monad) IntSet
+-- (3.5 s) < IntMap (4 s) < Sequence (10 s) Sequence not very good,
+-- because of sparse data (The ratio of data over holes is roughly
+-- 1/50) BitArray ... 150 ms; Bool STUArray ... 120 ms (likely binary
+-- packed)
 countUniq :: [Int] -> Int
-countUniq = foldUniq (0,target) (\c _->c+1) 0
+countUniq = foldUniq (0,target) (const . (+1)) 0
 
 prb87 :: IO Int
 prb87 = return (countUniq pps)
