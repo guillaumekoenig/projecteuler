5cd3283dd774947e650092d6de9608c515bd80e3
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2018-08-18 13:29:10 +0200

    Use Lib.Memoize everywhere; remove external memoize lib

--- src/Prb/Prb067.hs
+++ src/Prb/Prb067.hs
@@ -1,18 +1,22 @@
 module Prb.Prb067 (prb67) where
 
-import Data.Function.Memoize
+import Control.Monad.ST (runST, ST)
+import Data.Bits
 
--- Question : are the `l` memoizations shared ?
--- Or are they distinct, with recomputation of
--- the intermediate nodes for each of the `l`
--- cases ? I don't know how to figure that out.
-maxPath2 :: [[Int]] -> Int
-maxPath2 xs = maximum $ map (memoFix2 go (l-1)) [0..l-1]
-  where l = length xs
-        go _ 0 0 = xs!!0!!0
-        go f x 0 = f (x-1) 0 + xs!!x!!0
-        go f x y | x == y = f (x-1) (y-1) + xs!!x!!y
-        go f x y = max (f (x-1) (y-1)) (f (x-1) y) + xs!!x!!y
+import Lib.Memoize (memoize)
+
+maxPath2 :: [[Int]] -> (Int -> Int -> ST s Int) -> Int -> Int -> ST s Int
+maxPath2 tri _ 0 0 = pure $ tri!!0!!0
+maxPath2 tri f x 0 = f (x-1) 0 >>= pure . (+ tri!!x!!0)
+maxPath2 tri f x y | x == y = f (x-1) (y-1) >>= pure . (+ tri!!x!!y)
+maxPath2 tri f x y = liftM2 max (f (x-1) (y-1)) (f (x-1) y) >>= pure . (+ tri!!x!!y)
+
+fwrd :: [[Int]] -> (Int -> ST s Int) -> Int -> ST s Int
+fwrd tri f xy = maxPath2 tri (back f) x y
+  where (x,y) = (xy.&.0x7f,shiftR xy 7.&.0x7f)
+
+back :: (Int -> ST s Int) -> Int -> Int -> ST s Int
+back f x y = f $ (shiftL y 7).|.x
 
 parse :: String -> IO [[Int]]
 parse filename = do
@@ -20,4 +24,8 @@ parse filename = do
   return $ map (map read . words) $ lines c
 
 prb67 :: IO Int
-prb67 = parse "data/p067_triangle.txt" >>= return . maxPath2
+prb67 = do tri <- parse "data/p067_triangle.txt"
+           return $ runST $ memoize (0,2^14-1) (fwrd tri) $
+             \fwrd' -> do
+               let memoMP2 = back fwrd'; l = length tri
+               liftM maximum $ mapM (\y->memoMP2 (l-1) y) [0..l-1]
--- src/Prb/Prb081.hs
+++ src/Prb/Prb081.hs
@@ -1,16 +1,27 @@
 module Prb.Prb081 (prb81) where
 
-import Data.Function.Memoize
+import Control.Monad.ST (runST, ST)
+import Data.Bits
 
-minPath :: [[Int]] -> ((Int,Int) -> Int) -> (Int,Int) -> Int
-minPath matrix _ (0,0) = matrix !! 0 !! 0
-minPath matrix f (i,0) = matrix !! i !! 0 + f (i-1,0)
-minPath matrix f (0,j) = matrix !! 0 !! j + f (0,j-1)
-minPath matrix f (i,j) = matrix !! i !! j + min left top
+import Lib.Memoize (memoize)
+
+minPath :: [[Int]] -> ((Int,Int) -> ST s Int) -> (Int,Int) -> ST s Int
+minPath matrix _ (0,0) = pure $ matrix !! 0 !! 0
+minPath matrix f (i,0) = liftM (matrix !! i !! 0 +) (f (i-1,0))
+minPath matrix f (0,j) = liftM (matrix !! 0 !! j +) (f (0,j-1))
+minPath matrix f (i,j) = liftM (matrix !! i !! j +) (liftM2 min left top)
   where left = f (i-1,j); top = f (i,j-1)
+
+fwrd :: [[Int]] -> (Int -> ST s Int) -> Int -> ST s Int
+fwrd matrix f xy = minPath matrix (back f) (x,y)
+  where (x,y) = (xy.&.0x7f,shiftR xy 7.&.0x7f)
+
+back :: (Int -> ST s Int) -> (Int,Int) -> ST s Int
+back f (x,y) = f $ (shiftL y 7).|.x
 
 prb81 :: IO Int
 prb81 = do
   contents <- readFile "data/p081_matrix.txt"
   let matrix = map (\line->read ("["++line++"]")) $ lines contents
-  return $ (memoFix $ minPath matrix) (79,79)
+  return $ runST $ memoize (0,2^14-1) (fwrd matrix) $
+    \fwrd' -> (back fwrd') (79,79)
