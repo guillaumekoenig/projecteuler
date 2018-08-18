3cabc4a1cdd599e865ca0fbfc31caa92bd6fa2cc
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2018-08-18 17:48:37 +0200

    Left align display output

--- src/Main.hs
+++ src/Main.hs
@@ -23,26 +23,30 @@ prbs = [prb1, prb2, prb3, prb4, prb5, prb6, prb7, prb8, prb9, prb10,
 
 runAll :: [IO Int] -> IO [(Int,NominalDiffTime)]
 runAll [] = return []
-runAll (p:ps) = do start <- getCurrentTime
-                   r <- p
-                   let !result = r
-                   end <- getCurrentTime
-                   let time = diffUTCTime end start
-                   rest <- runAll ps
-                   return $ (result,time):rest
+runAll (p:ps) = do
+  start <- getCurrentTime
+  r <- p
+  let !result = r
+  end <- getCurrentTime
+  let time = diffUTCTime end start
+  rest <- runAll ps
+  return $ (result,time):rest
 
 thd :: (a,b,c) -> c
 thd (_,_,x) = x
 
+alignleft :: Int -> String -> String
+alignleft n s = s ++ replicate (n-length s) ' '
+
 main :: IO ()
-main = do args <- getArgs
-          let is = if length args > 0 then map read args
-                   else [1 .. length prbs]
-          results <- runAll [prbs !! (i-1) | i<-is]
-          let labeled = zipWith (\a (b,c)->(a,b,c)) is results
-          let orderDesc = sortBy (flip $ comparing thd) labeled
-          let totalTime = sum $ map snd results
-          let pp (n,r,t) = "Prb #" ++ show (n::Int) ++ ": "
-                ++ show r ++ "    \t(" ++ show t ++ ")"
-          mapM_ (putStrLn . pp) orderDesc
-          putStrLn $ "\nTotal: " ++ show totalTime
+main = do
+  args <- getArgs
+  let ks = if null args then [1..length prbs] else map read args
+  results <- runAll [prbs !! (k-1) | k<-ks]
+  let labeled = zipWith (\a (b,c)->(a,b,c)) ks results
+      orderDesc = sortBy (flip $ comparing thd) labeled
+      totalTime = sum $ map snd results
+      pp (n,r,t) = alignleft 10 ("Prb #" ++ show (n::Int) ++ ":")
+        ++ alignleft 16 (show r) ++ "(" ++ show t ++ ")"
+  mapM_ (putStrLn . pp) orderDesc
+  putStrLn $ "\nTotal: " ++ show totalTime
