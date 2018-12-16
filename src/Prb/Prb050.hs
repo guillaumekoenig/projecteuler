5b6f3bb97d1b999ab952312f333ad14f08977101
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2018-12-16 08:59:03 +0100

    Rewrite problem 50 to be faster

--- src/Prb/Prb050.hs
+++ src/Prb/Prb050.hs
@@ -2,14 +2,16 @@ module Prb.Prb050 (prb50) where
 
 import Lib.IsPrime
 
-maxSum :: [Int] -> (Int,Int) -> (Int,Int,[Int]) -> Int
-maxSum ps (minlen,s) (acc,acclen,rest)
-  | null rest = s
-  | acc >= 1000000 = maxSum (tail ps) (minlen,s) newacc
-  | otherwise = maxSum ps newminlen (acc+head rest,acclen+1,tail rest)
-  where newacc = (sum $ take minlen $ tail ps,minlen,drop minlen $ tail ps)
-        newminlen = if isPrime acc then (acclen,acc) else (minlen,s)
+maxPrimeSum :: [Int] -> [Int] -> (Int,Int) -> (Int,Int) -> Int
+maxPrimeSum [] _  _ (_,a) = a
+maxPrimeSum (_:ps) [] _ y = maxPrimeSum ps ps (0,0) y
+maxPrimeSum (p:ps) (q:qs) (m,acc) (n,a)
+  | acc==0 && (n+1)*p+n*(n+1) > 1000000 = a
+  | acc+q > 1000000 = maxPrimeSum ps ps (0,0) (n,a)
+  | otherwise = maxPrimeSum (p:ps) qs (m+1,acc+q)
+                (if isPrime (acc+q) && m+1>n
+                  then (m+1,acc+q) else (n,a))
 
 prb50 :: IO Int
-prb50 = return $ maxSum ps (1,0) (0,0,ps)
-  where ps = filter isPrime [2..1000000]
+prb50 = return $ maxPrimeSum ps ps (0,0) (0,0)
+  where ps = primesTo 1000000
