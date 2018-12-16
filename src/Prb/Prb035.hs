86c385cd6a3103120d02e0679a88c302ab733233
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2018-12-16 10:42:50 +0100

    Speed up problem 35

--- src/Prb/Prb035.hs
+++ src/Prb/Prb035.hs
@@ -7,6 +7,10 @@ rotations n = let a = show n
                   cut_a = \x -> let c = splitAt x a in snd c ++ fst c
                in map (read . cut_a) [1..length a]
 
+quick :: Int -> Bool
+quick n = n == 2 || n == 5 ||
+          (not $ any (\d -> elem d $ show n) "024568")
+
 prb35 :: IO Int
 prb35 = let circprime n = and $ map isPrime (rotations n)
-         in return $ length $ filter (\n->isPrime n && circprime n) [2..1000000]
+         in return $ length $ filter circprime $ filter quick $ primesTo 1000000
