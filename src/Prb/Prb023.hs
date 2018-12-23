3c46d8fa8343628bacda9a3a3c7a40e1c12d57b9
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2018-12-23 20:41:50 +0100

    Slightly faster problem 23

--- src/Prb/Prb023.hs
+++ src/Prb/Prb023.hs
@@ -1,21 +1,23 @@
 module Prb.Prb023 (prb23) where
 
 import Lib.IsPrime (divisors)
-import Data.Array.ST
-import Control.Monad.ST
+import Data.Array.ST (newArray, STUArray)
+import Data.Array.Base (unsafeRead, unsafeWrite)
+import Control.Monad.ST (ST, runST)
+import Control.Monad (foldM)
 import Data.List (tails)
 
-sumOfAbundants :: [Int] -> Int -> [Bool]
+sumOfAbundants :: [Int] -> Int -> Int
 sumOfAbundants as lim = runST $ do
   -- Ok ST is black magic to me. In any case, arrays
   -- in this setting are way faster than plain old lists.
   arr <- newArray (1,lim) False :: ST s (STUArray s Int Bool)
   let getSums tailAs = takeWhile (<= lim) $ map (head tailAs+) tailAs
-  mapM_ (\x -> writeArray arr x True) . concatMap getSums $ tails as
-  getElems arr
+  mapM_ (\x -> unsafeWrite arr x True) . concatMap getSums $ tails as
+  let f acc i = do {b <- unsafeRead arr i; return (if b then acc else acc+i)}
+  foldM f 0 [1..lim]
 
 prb23 :: IO Int
-prb23 = return $ foldr f 0 $ zip [1..] (sumOfAbundants as lim)
-  where f (x,isSum) acc = acc + if not isSum then x else 0
-        as = filter (\x -> sum (divisors x) > x) [12..lim]
+prb23 = return (sumOfAbundants as lim)
+  where as = filter (\x -> sum (divisors x) > x) [12..lim]
         lim = 28123
