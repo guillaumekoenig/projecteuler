a6e5d7a26761426e289922965664612d72e6e32d
Guillaume Koenig <guillaume.edward.koenig@gmail.com>
2019-05-06 12:32:47 +0200

    Add quickcheck property for isqrt

--- src/Lib/Isqrt.hs
+++ src/Lib/Isqrt.hs
@@ -3,6 +3,11 @@ module Lib.Isqrt (isqrt, isSquare, isqrtn) where
 isqrt :: Int -> Int
 isqrt = floor . (sqrt :: Double -> Double) . fromIntegral
 
+-- known to fail for some large Int values, eg 2^62-1
+prop_Isqrt :: Int -> Bool
+prop_Isqrt n = let r = isqrt n
+               in r*r<=n && n<(r+1)*(r+1)
+
 isSquare :: Int -> Bool
 isSquare n = let r = isqrt n in r*r == n
 
